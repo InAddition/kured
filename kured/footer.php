6dbba7c1d3d386e1395822d22ae7899e92fa2323
Thomas Weaver <tweaver75@gmail.com>
2018-07-15 20:43:43 -0600

    Ajust cart styles after adding coupon extension plugin

--- kured/footer.php
+++ kured/footer.php
@@ -91,21 +91,23 @@
 		<?php global $asset_v; ?>
 		<script src="<?php bloginfo('template_url'); ?>/js/main.min.js?v=<?php echo $asset_v; ?>"></script>
 		<!-- Facebook Pixel Code -->
+		<!-- Facebook Pixel Code -->
 		<script>
-		  !function(f,b,e,v,n,t,s)
-		  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-		  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-		  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-		  n.queue=[];t=b.createElement(e);t.async=!0;
-		  t.src=v;s=b.getElementsByTagName(e)[0];
-		  s.parentNode.insertBefore(t,s)}(window, document,'script',
-		  'https://connect.facebook.net/en_US/fbevents.js');
-		  fbq('init', '688873701283171');
-		  fbq('track', 'PageView');
+		 !function(f,b,e,v,n,t,s)
+		 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+		 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+		 if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+		 n.queue=[];t=b.createElement(e);t.async=!0;
+		 t.src=v;s=b.getElementsByTagName(e)[0];
+		 s.parentNode.insertBefore(t,s)}(window, document,'script',
+		 'https://connect.facebook.net/en_US/fbevents.js');
+		 fbq('init', '233317574132503');
+		 fbq('track', 'PageView');
 		</script>
 		<noscript><img height="1" width="1" style="display:none"
-		  src="https://www.facebook.com/tr?id=688873701283171&ev=PageView&noscript=1"
+		 src="https://www.facebook.com/tr?id=233317574132503&ev=PageView&noscript=1"
 		/></noscript>
+
 		<!-- End Facebook Pixel Code -->
 		<?php wp_footer(); ?>
 	</body>
